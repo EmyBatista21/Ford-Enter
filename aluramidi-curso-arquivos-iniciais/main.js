0700e93742dcef769ff2d5ca9be381259a94c8a0
EmyBatista21 <emybatista159@gmail.com>
2025-08-14 11:32:46 -0300

    Adiconando arquivos

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,46 @@
+function tocarSomPom(){
+    
+}
+
+/*
+function tocarSomClap(){
+    document.querySelector("#som_tecla_clap").play();
+}
+
+
+function tocarSomTim(){
+    document.querySelector("#som_tecla_tim").play();
+}
+
+function tocarSomPuff(){
+    document.querySelector("#som_tecla_puff").play();
+}
+
+function tocarSomSplash(){
+    document.querySelector("#som_tecla_splash").play();
+}
+
+function tocarSomToim(){
+    document.querySelector("#som_tecla_toim").play();
+}
+
+function tocarSomPsh(){
+    document.querySelector("#som_tecla_psh").play();
+}
+
+function tocarSomTic(){
+    document.querySelector("#som_tecla_tic").play();
+}
+
+function tocarSomTom(){
+    document.querySelector("#som_tecla_tom").play();
+}
+*/
+
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+
+while(){
+    listaDeTeclas[0] = tocarSomPom;
+}
